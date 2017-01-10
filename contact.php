6df95e8ce551442ef3fe14aa1c7fc82fee7dc702
aminovich <amine.radh@hotmail.fr>
2017-01-10 14:27:36 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,23 @@
 
+<?php include("template.php");?>
+
+
+<div class="img">
+    <img src="eric.jpg">
+<p> blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+
+
+
+
+    <img src="amine.jpg">
+<p> blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>  
+
+
+
+
+
+
+    <img src="sylvain.jpg">
+<p> blaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+</div>
+
