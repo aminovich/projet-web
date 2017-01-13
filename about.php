d6077822b36aa4ace6e2f45a2900d04df6150326
aminovich <amine.radh@hotmail.fr>
2017-01-13 13:20:20 +0100

    Update about.php

---
+++ about.php
@@ -0,0 +1,26 @@
+<?php include("template.php");?>
+
+
+
+
+<div id="centre">
+		
+	<div class= "about">
+		<h1> About </h1>
+		<p> Notre équipe est constituée par des étudiants du master AMI2B de l'Université Paris-Sud. Ce projet s'inscrit dans le cadre de l’UE "Programmation Web" dirigé par Mr Olivier Lespinet. </p>
+		<p> Il consiste en la création d’un site internet pour la visualisation de données récupérées d’une banque de données : la Genome OnLine Database du Joint Genome Institute. </p>
+	</div> 
+
+
+	<div class="logos">
+
+		<div class="img5">   
+        	  <a href="https://gold.jgi.doe.gov/"> <img src="jgi.jpg"> </a>
+        </div>
+
+		<div class="img4">   
+        	  <a href="http://www.bibs.u-psud.fr/m2_ami2b.php"> <img src="psud.png"> </a>
+        </div>
+
+    </div>
+</div>
